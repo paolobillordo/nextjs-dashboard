ea6db8c1de7aa928a9beab3c25d987e76a24d6a4
Paolo Billordo <paolobillordo@gmail.com>
2024-12-12 01:15:50 -0300

    first commit

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { fetchRevenue } from "../lib/data"
+
+export default async function Page() {
+    const revenue = await fetchRevenue()
+    console.log(revenue)
+    return <p>Dashboard Page</p>
+}
